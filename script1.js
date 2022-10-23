679d9a2aca8dc260eb5678482a3b21bb99fc5cec
UsamarkKhan <usamarkkhan@gmail.com>
2022-10-24 00:30:58 +0500

    js assignment done

--- script1.js
+++ script1.js
@@ -1,6 +1,60 @@
-let valu1 = prompt("Enter First Number")
-let valu2 = prompt("Enter second Number")
-a = Number(valu1);
-b = Number(valu2);
-result = a + b
+// alert("welcome to JS Land....\n Happy Coding!")
+
+/*var studentname = "Mohammad Usama Khan";
+var studentage = 23
+var studentcourse = "Web 3.0 and metaverse"
+alert(studentname)
+alert(studentage)
+alert(studentcourse)*/
+
+//var myemail = "usamarkkhan1122@gmail.com";
+//alert("My Email is "+ myemail )
+//document.write("Yah! I can write HTML through JS")
+
+// var age = 23
+// alert("I am "+ age + " year old");
+
+// var birthyear = 1998;
+// document.write("My Birth Year is " + birthyear)
+
+// document.write("Variable names can only contain $ and _,For example : $my_1stvariable <br> Variable must begin with a letter , $ or _ <br> Varitable names are case sensitive Variable names should notbe JS keywords")
+
+// var a = 50
+// var b = 90
+// var c = a + b
+// d = a - b
+// e = a * b
+// f = a / b
+// g = a % b
+// document.write("The sum of "+ a  + " and "+  b + " is " + c + " <br>The subtraction of " + a + " and " + b +" is " + d + "<br>The Mulltiplication of "+ a + " and " + b + " is " + e + "<br> The Division of " + a + " and " + b + "is" + f + "<br>The Modulas of " + a + " and " + b + " is " + g);
+
+// var first;
+// document.write(" Value after variable declaration is: " + first + ".<br>")
+// first = 5;
+// document.write("Initial value : " + first)
+// ++first
+// document.write("<br>Value after increment is: " + first)
+// first = first + 7 
+// document.write("<br>Value after addition is: " + first)
+// --first
+// document.write("<br>Value after decrement is: " + first)
+// first = first % 3
+// document.write("<br>Value after modulas is: " + first)
+
+// var cel = 25 ;
+// var far = 70 ;
+// celf = (far - 32)*5/9
+// farc = (cel*9/5)+32
+// document.write(cel+"<sup>0</sup>C is " + farc + "<br>" )
+// document.write(far+"<sup>0</sup>C is " + celf )
+
+// var cal = (10 * 155) + (25*41);
+// document.write("Total Currency is PKR is " + cal); 
+
+// var fnum = 10;
+// document.write("The Value of A is :" + fnum + "<br><br> The Value of ++A is :" + ++fnum + "<br>Now The Value of A is :" + fnum +"<br><br> The Value of A++ is :" + fnum++ + "<br>Now The Value of A is :" + fnum + "<br> <br> The Value of --A is :" + --fnum + "<br> The Value of A is :" + fnum + "<br><br> The Value of A-- is :" + fnum-- + "<br>Now The Value of A is :" + fnum )
+
+var a = 2, b = 1;
+var result = --a - --b + ++b + b--;
 document.write(result)
+
